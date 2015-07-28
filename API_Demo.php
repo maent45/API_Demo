5fef22e86221be3a7e0d269940884b0db4a40000
Maen Terawasi <maent45@gmail.com>
2015-07-28 20:26:49 +1200

    Small_API_App
    messing around with integrating google maps and instagram API.. not much
    code for now but more will be added.

---
+++ API_Demo.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by Maen Terawasi.
+ * User: 21103436
+ * Date: 28/07/2015
+ * Time: 6:01 PM
+ */
+?>
+
+<!DOCTYPE html>
+<head>
+    <title>API Demo</title>
+
+    <meta name="viewport" content="width=device-width; initial-scale = 1.0; maximum-scale=1.0; user-scalable=no" />
+    <link href="css/bootstrap.min.css">
+    <link href="css/custom.css">
+</head>
+<body>
+    <div class="container">
+        <form role="form">
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" id="email" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label for="pwd">Password:</label>
+                <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+            </div>
+            <div class="checkbox">
+                <label><input type="checkbox"> Remember me</label>
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+    </div>
+</body>
